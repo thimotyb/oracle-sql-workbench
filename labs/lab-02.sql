8a3a960821efb8752a0bd66c120e91cdddef0a08
thimotyb <thimoty@thimoty.it>
2024-02-15 15:22:15 +0100

    Create lab-02.sql

---
+++ labs/lab-02.sql
@@ -0,0 +1,21 @@
+# Solution 2-1
+SELECT last_name, job_id, salary AS Sal FROM employees;
+
+DESCRIBE departments
+
+SELECT * FROM departments;
+
+DESCRIBE employees
+
+SELECT employee_id, last_name, job_id, hire_date StartDate FROM employees;
+
+SELECT DISTINCT job_id FROM employees;
+
+SELECT employee_id "Emp #", last_name "Employee", job_id "Job", hire_date "Hire Date"
+FROM employees;
+
+SELECT last_name||', '||job_id "Employee and Title" FROM employees;
+
+SELECT employee_id || ',' || first_name || ',' || last_name || ',' || email || ',' || phone_number || ','|| job_id || ',' || manager_id || ',' || hire_date || ',' || salary || ',' || commission_pct || ',' ||
+department_id THE_OUTPUT
+FROM employees;
